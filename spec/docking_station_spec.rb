953195b98a36653efa2fb7274e6081644fbb0c15
Luke Czoboriek <LC169782@protonmail.com>
2021-04-28 12:00:34 +0100

    updated

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,6 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -23,5 +19,20 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
+
+  describe '#release_bike' do # This bit ere makes the bike equal to the bike your docking, weird
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
   
+
+  
+    end
+  end
+  describe '#release_bike' do # then this bit ere raises an error if no bikes are available
+    it 'raises error when no bikes are there' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end    
+  end
 end
